4a30f0bf8c3422819f6f5bdc28a2aad4125ca97d
Wellington Moreno <JWellington.Moreno@Gmail.com>
2026-01-20 11:12:00 -0800

    Adds base UIKit project

---
+++ ModernImageSearchUIKit/ModernImageSearchUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModernImageSearchUIKit
+//
+//  Created by Wellington Moreno on 1/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
